6bf496d5f10ff44e1268e65992979094cfd6573c
hoosin <hoosin@yeah.net>
2014-12-15 14:24:47 +0800

    update

--- bin/build-dependency-css.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-node_modules/.bin/cleancss \
-  bower_components/bootstrap/dist/css/bootstrap.min.css \
-  bower_components/nprogress/nprogress.css \
-  bower_components/highlightjs/styles/github.css \
-  -o dist/airpub-dependencies.min.css \
-  --s0 \
-  --debug
