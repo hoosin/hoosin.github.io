718f0c9775f63462405e52e748b1d97216de600f
sin01.hu <sin01.hu@vipshop.com>
2016-11-30 11:15:34 +0800

    release

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,1 @@
-console.log("%c\n     ","font-size:81px;background:url('//javascript.mom/dist/WechatIMG2.jpeg') no-repeat 0 0");
+console.log("%c\n     ","font-size:81px;background:url('http://javascript.mom/dist/WechatIMG2.jpeg') no-repeat 0 0");
