795f031284651cee709160b9be2a9537c23d1487
sin01.hu <sin01.hu@vipshop.com>
2016-11-30 11:16:53 +0800

    release

--- dist/bundle.js
+++ dist/bundle.js
@@ -44,7 +44,7 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	console.log("%c\n     ","font-size:81px;background:url('//javascript.mom/dist/WechatIMG2.jpeg') no-repeat 0 0");
+	console.log("%c\n     ","font-size:81px;background:url('http://javascript.mom/dist/WechatIMG2.jpeg') no-repeat 0 0");
 
 
 /***/ }
