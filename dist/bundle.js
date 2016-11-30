4968078e0a0f02ff791bb3d574d3e007ad5a4f3b
sin01.hu <sin01.hu@vipshop.com>
2016-11-30 11:24:44 +0800

    release

--- dist/bundle.js
+++ dist/bundle.js
@@ -44,8 +44,15 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	console.log("%c\n     ","font-size:81px;background:url('http://javascript.mom/dist/WechatIMG2.jpeg') no-repeat 0 0");
+	var app = {};
 
+
+	app.consoler = function (protocol) {
+	    console.log('%c\n     ', 'width:752px;height:974px;background:url('+protocol+'"//javascript.mom/dist/WechatIMG2.jpeg") no-repeat 0 0');
+	}
+
+
+	app.consoler(location.protocol);
 
 /***/ }
 /******/ ]);
--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,9 @@
-console.log("%c\n     ","font-size:81px;background:url('http://javascript.mom/dist/WechatIMG2.jpeg') no-repeat 0 0");
+var app = {};
+
+
+app.consoler = function (protocol) {
+    console.log('%c\n     ', 'width:752px;height:974px;background:url('+protocol+'"//javascript.mom/dist/WechatIMG2.jpeg") no-repeat 0 0');
+}
+
+
+app.consoler(location.protocol);
