b2b2c24945a2959deeb6dbd1a4b68761a5f41340
sin01.hu <sin01.hu@vipshop.com>
2016-11-30 11:30:08 +0800

    release

--- src/entry.js
+++ src/entry.js
@@ -2,7 +2,7 @@ var app = {};
 
 
 app.consoler = function (protocol) {
-    console.log('%c\n     ', 'font-size:500px;background:url('+protocol+'"//javascript.mom/dist/WechatIMG2.jpeg") no-repeat 0 0');
+    console.log('%c', 'padding:100px 300px;line-height:150px;background:url('+protocol+'"//javascript.mom/dist/WechatIMG2.jpeg") no-repeat;');
 }
 
 
