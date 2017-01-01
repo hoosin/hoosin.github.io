8c8abe221812b22a02e3c907f84714d32dee7342
hoosin <sin01.hu@vipshop.com>
2017-01-01 13:42:39 +0800

    release

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,36 @@
 var webpack = require('webpack')
 var path = require('path');
 
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/entry.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {test: /\.css$/, loader: 'style!css'}
-        ]
-    }
+  entry: './src/entry.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: '../index.html',
+      template: './src/index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+  ]
 }
